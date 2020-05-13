4aa61a07ced1925ab5055112fffec91a77c7c708
bAbybLAckbeAr <sam.wendt333@gmail.com>
2020-05-13 14:25:58 -0400

    fixed bugs in code. Can we use '+' and '-' operators?

--- ALU.vhd
+++ ALU.vhd
@@ -5,11 +5,11 @@ entity ALU32 is
 	  B : in std_logic_vector (31 downto 0);
           OP: in std_logic_vector (1 downto 0);
           Result: out std_logic_vector (31 downto 0);
-	  Equal: out std_logic;
+	  Equal: out std_logic);
              
 end ALU32;
 
-architecture Behavioral of ALU is
+architecture Behavioral of ALU32 is
 begin
 process(A, B, OP)
 begin
@@ -19,13 +19,19 @@ when "00" =>Result<= A + B; --addition
  
 when "01" =>Result<= A - B; --subtraction
  
-when "10" => Result <= NULL;  --Do nothing
+when "10" => --Do nothing
  
 when "11" => -- Set on Equal
- 
- when others =>
- NULL;
+	if A = B then
+		Equal <= '1';
+	else
+		Equal <= '0';
+end if; 
+--when others => 
+	
+-- NULL;
 end case;
   
 end process;
  
+end Behavioral;
